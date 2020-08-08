80126eef9b0570f9d830fbb55527da702f735bd6
Madhurya <65159266+madhurya-ops@users.noreply.github.com>
2020-08-08 08:17:32 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var helicopterIMG, helicopter, package,packageIMG;
+var packageBody,ground, dZone;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	rectMode(CENTER);
+	
+
+	package = createSprite(width/2, 80, 10,10);
+	package.addImage(packageIMG)
+	package.scale=0.2
+
+	helicopter=createSprite(width/2, 200, 10,10);
+	helicopter.addImage(helicopterIMG)
+	helicopter.scale=0.6
+
+	dZone = new Dzone(width/2, 690, 200,20);
+	
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 )
+		var options = {
+			restitution:0.5,
+			isStatic: false}
+		
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {
+		isStatic:true} );
+
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+
+  package.x= packageBody.position.x 
+  package.y= packageBody.position.y 
+
+  dZone.display();
+ 
+  drawSprites();
+
+
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody, false);
+    
+  }
+}
+
+
+
